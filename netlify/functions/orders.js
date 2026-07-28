c72d68e86c0b5d25d58c8842b8feecfeb49a3680
2Hg-tndms <sandboxlikethe@gmail.com>
2026-07-28 15:59:16 +0900

    Update orders.js

--- netlify/functions/orders.js
+++ netlify/functions/orders.js
@@ -1,10 +1,11 @@
-const { getStore } = require('@netlify/blobs');
+const { getStore, connectLambda } = require('@netlify/blobs');
 
 function uid() {
   return Date.now().toString(36) + Math.random().toString(36).slice(2, 7);
 }
 
 exports.handler = async (event) => {
+  connectLambda(event);
   const headers = { 'Content-Type': 'application/json' };
   const store = getStore('rodem-orders');
 
